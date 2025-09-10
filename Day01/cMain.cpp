e1e80438b2db866f92fb769e96ddcf41afaae968
lecture <ksw617@gmail.com>
2025-09-10 20:47:27 +0900

    Update cMain.cpp

--- Day01/cMain.cpp
+++ Day01/cMain.cpp
@@ -1,15 +1,56 @@
 #include <stdio.h>
-#include <Windows.h>
+#include <Windows.h>  
+
+struct Obj
+{
+	int x;
+	int y;
+	const char* shape;
+};
+
+void SetPosition(int x, int y)
+{
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
 
 int main()
 {
+	Obj player;
+	player.x = 10;
+	player.y = 10;
+	player.shape = "¿Ê";
+
+	Obj enemy;
+	enemy.x = 20;
+	enemy.y = 20;
+	enemy.shape = "º¿";
+
 	while (true)
 	{
 		system("cls");
 
-		printf("Hello world");
+		if (GetAsyncKeyState(VK_LEFT))
+		{
+			player.x--;
+		}
 
-		Sleep(50);
+
+		if (GetAsyncKeyState(VK_RIGHT))
+		{
+			player.x++;
+		}
+
+
+		SetPosition(player.x, player.y);
+		printf(player.shape);
+
+		SetPosition(enemy.x , enemy.y);
+		printf(enemy.shape);
+
+		Sleep(20);	  
 
 	}
 	return 0;
